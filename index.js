e1e9ddf53e73a8abe5645a28e61b40bf9c8a6963
Nate Brock <natebrock@gmail.com>
2016-11-21 19:51:08 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+/*jslint esversion: 6 */
+/*jslint node: true */
+'use strict';
+
+const _ = require('underscore');
+
+module.exports = function Election() {
+  const data = require('./lib/2012pres.json');
+
+  this.getState = function (states) {
+    let stateData;
+
+    if (!_.isArray(states)) {
+      if (typeof states === 'undefined') {
+        states = [];
+      } else {
+        states = states.split();
+      }
+    }
+
+    let getAll = states.length === 0 ? true : false;
+
+    stateData = _.filter(data.states, function (state) {
+      let match = _.contains(states, state.stateAbv);
+      return (getAll || match);
+    });
+
+    stateData = stateData.map(function (state) {
+      return _.pick(state, 'electoralVotesTotal', 'state');
+    });
+
+    return stateData;
+  };
+};
